0af2064da6a249c89debeeb09c963ea2ba05ad16
unknown <luisf.rossi@gmail.com>
2013-08-07 11:00:06 -0300

    Changed addJoint to support vararg

--- Joint.m
+++ Joint.m
@@ -1,4 +1,5 @@
 function obj = Joint(dh)
     obj.DH = dh;
     obj.angle = 0;
+    obj.mass = 0;
 end
--- addJoint.m
+++ addJoint.m
@@ -1,6 +1,19 @@
-function obj = addJoint( M, alpha ,theta, a, offset)
+%% ADDJOINT Add a joint to a manipulator
+% ========================================================================
+function obj = addJoint( M, alpha, theta, a, offset, varargin)
     dh =  struct('alpha', alpha, 'theta', theta, 'a', a, 'offset', offset);
     joint = Joint(dh);
+    argl = length(varargin);
+    if(argl > 1)
+        if rem(argl,2) ~= 0
+            error('Optional inputs must be in format ''variable name'',variable value')
+        end
+        for ii = 1:2:argl
+            if strcmp(varargin{ii},'Mass')
+                joint.mass = varargin{ii+1};
+            end
+        end
+    end
     M.joints = [M.joints; joint];
     obj = M;
 end
