4e6161e9b16fd655edf18500dcf82c02334b86b2
unknown <luisf.rossi@gmail.com>
2013-08-08 18:42:32 -0300

    Implemented kinIK with both Jacobian pseudo Inverse  and Jacobian Transpose. For the last a proper alpha optimization still need to be implemented.

--- kineIK.m
+++ kineIK.m
@@ -1,7 +1,81 @@
-function joints_ref = kineIK(joints, pos_ref) 
+function joints_ref = kineIK(joints, pos_ref, varargin) 
 %http://sourceforge.net/p/robotjoint/code/ci/master/tree/src/RobotManipulator.cxx#l899
-joints_ref = zeros(3,1);
-
-disp('TODO: Implement kineIK')
-
+    %default values
+    maxiter = 1000;
+    maxposerr = 0.005;
+    maxroterr = 0.0001;
+    kTheta = 1;
+    kX = 0.1;
+    mode = 0;
+    
+    %local variables
+    DOF = size(joints,1);
+    argl = length(varargin);
+    error = zeros(6,1);
+    joints_ref = zeros(DOF,1);
+    
+    % Feed optional configurations
+    if(argl > 1)
+        if rem(argl,2) ~= 0
+            error('Optional inputs must be in format ''variable name'',variable value')
+        end
+        for ii = 1:2:argl
+            if strcmp(varargin{ii},'maxiter')
+                maxiter = varargin{ii+1};
+            end
+            if strcmp(varargin{ii},'kX')
+                kX = varargin{ii+1};
+            end
+            if strcmp(varargin{ii},'kTheta')
+                kTheta = varargin{ii+1};
+            end
+            if strcmp(varargin{ii},'maxposerr')
+                maxposerr = varargin{ii+1};
+            end
+            if strcmp(varargin{ii},'maxroterr')
+                maxroterr = varargin{ii+1};
+            end
+            if strcmp(varargin{ii},'mode')
+                if(strcmp(varargin{ii+1},'pinv'))
+                    mode = 0;
+                end
+                if(strcmp(varargin{ii+1},'transp'))
+                    mode = 1;
+                end
+            end
+        end
+    end
+    for cntr = 1:maxiter
+        % Feed Joints Angles
+        for i=1:DOF
+            joints(i).angle = joints_ref(i,1);
+        end
+        % Calc Jacobian
+        jac = kineJacobian(joints);
+        % Calc PseudoInverse
+        switch mode
+            case 0
+                %use Pseudo Inverse
+                ijac = pinv(jac);
+            case 1
+                %use Tanspose (need to implement optimal alpha)
+                ijac = jac';
+            otherwise
+                ijac = pinv(jac);
+        end
+        % Get TransForm Matrix
+        fk = forwardKine(joints);
+        % Get Translation
+        x = fk(1:3,4);
+        % Calc the error
+        error(1:3) = (pos_ref - x);
+        if(max(abs(error(1:3))) < maxposerr && max(abs(error(4:6))) < maxroterr)
+            break;
+        end
+        error(1:3) = kX*error(1:3);
+        % Calc new joint angles
+        joints_ref  = joints_ref +ijac*error;
+        % Lets just remove the accumulated angle in excess
+        joints_ref = mod(joints_ref, 2*pi);
+    end
 end
--- kineIKTest.m
+++ kineIKTest.m
@@ -1,4 +1,5 @@
 clear all;
+close all;
 
 %Create a Manipulator
 M = Manipulator();
@@ -7,27 +8,43 @@ M = Manipulator();
 time_step = 0.01;
 end_time = 10;
 
-M = addJoint( M,  0 , 0, 0.1 , 0);
-M = addJoint( M,  0 , 0, 0.1 , 0);
+M = addJoint( M,  0 , 0, 0.2 , 0);
+M = addJoint( M,  0 , 0, 0.2 , 0);
+M = addJoint( M,  0 , 0, 0.2 , 0);
+%M = addJoint( M,  0 , 0, 0.2 , 0);
 
 position_ref = zeros(3,1);
 
 %for plotting data later
 positionRef = [];
 positionResponse_log = [];
+positionErr_log = [];
 
 %generate position reference
-position_ref = [0.1; 0.1; 0]
-	
-%there will also be a comIK() later on, so thats why I choose this name
-jointAnglesRef = kineIK(M.joints, position_ref);
+DOF = size(M.joints,1);
+%Lets iter from some range of positions
+for y = 0:0.01:DOF*0.2
+    
+    position_ref = [0 ; y; 0];
 
-M.joints(1).angle = jointAnglesRef(1);
-M.joints(2).angle = jointAnglesRef(2);
+    %there will also be a comIK() later on, so thats why I choose this name
+    % Testing pseudo inverse Jacobian and transpose Jacobian
+    
+    jointAnglesRef = kineIK(M.joints, position_ref, 'maxiter', 1000, 'kX', 1, 'maxposerr', 0.0005, 'mode', 'transp');
+    %jointAnglesRef = kineIK(M.joints, position_ref, 'maxiter', 1000, 'kX', 0.5, 'maxposerr', 0.0005, 'mode', 'pinv');
+    
+    for i=1:DOF
+        M.joints(i).angle = jointAnglesRef(i);
+    end
 
-position_response = forwardKine(M.joints)(1:3,4)
+    fk = forwardKine(M.joints);
+    position_response = fk(1:3,4);
+    positionErr = position_ref - position_response;
+    positionErr_log = [positionErr_log; positionErr'];
+    positionResponse_log = [positionResponse_log; position_response'];
+end
 
-
+plot(positionErr_log);
 disp('position_ref and position_response should be almost the same')
 disp('watch out for singularities, the IK algo wont be able to solve if a unreachable pos_ref is given')
 	
