c1116e2a81ac68c718565e88f4688514d8a52bca
unknown <luisf.rossi@gmail.com>
2013-09-03 11:22:25 -0300

    Added combined comJacobian option

---
+++ calcRobotCOM.m
@@ -0,0 +1,12 @@
+function COM = calcRobotCOM(R)
+%CALCROBOTCOM Summary of this function goes here
+%   Detailed explanation goes here
+    nM = size(R.manipulators, 1);
+    for i=1:nM
+        iCOM = calcCOM(R.manipulators(i).joints);
+        COM(1:3) = COM(1:3)*COM(4)+iCOM(1:3)*iCOM(4);
+        COM(4) = COM(4)+iCOM(4);
+        COM(1:3) = COM(1:3)/COM(4);
+    end
+end
+
--- comJacobian.m
+++ comJacobian.m
@@ -1,6 +1,26 @@
-function Jac = comJacobian(joints) 
+function Jac = comJacobian(joints, varargin) 
 %see http://www.elysium-labs.com/robotics-corner/learn-robotics/biped-basics/com-jacobian/
 %http://sourceforge.net/p/robotjoint/code/ci/master/tree/src/RobotManipulator.cxx#l272
+    useRobot = false;
+    AuxCOM = zeros(4,1);
+    argl = length(varargin);   
+    if(argl > 1)
+        if rem(argl,2) ~= 0
+            error('comJacobian: Optional inputs must be in format ''variable name'',variable value')
+        end
+        for ii = 1:2:argl
+            if strcmp(varargin{ii},'Robot')
+                R = varargin{ii+1};
+                useRobot = true;
+            end        
+        end
+    end
+    if(useRobot == true)
+        RobotCOM = calcRobotCOM(R);
+        ManCOM = calcCOM(joints);
+        AuxCOM(4) = RobotCOM(4) - ManCOM(4);
+        AuxCOM(1:3) = (RobotCOM(4)*RobotCOM(1:3)-ManCOM(4)*ManCOM(1:3))/AuxCOM(4);
+    end
     DOF = size(joints,1); %the number of joints
     mass = 0;
     for i=1:DOF
@@ -18,6 +38,9 @@ function Jac = comJacobian(joints)
         end
         zn = rotn(:,3);
         pCOM = calcCOM(joints, i);
+        if(useRobot == true)
+           pCOM(1:3) = (pCOM(1:3)*pCOM(4)+AuxCOM(1:3)*AuxCOM(4))/(AuxCOM(4)+pCOM(4));
+        end
         xn = -pL + pCOM(1:3);
         Jac(:,i) = vertcat((pCOM(4)/mass)*cross(zn,xn),zn);
     end
